ebb8b00a91a30f20c14ba947d6ab5317ff3f0362
icartadmin <icartmail01@gmail.com>
2018-01-27 13:38:28 +0530

    My First Commit to GIT

---
+++ JavaTutorial/src/org/basics/one/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.basics.one;
+
+//The Class (Blueprint) to build an Object. It defines the working of an Object.
+public class HelloWorld {
+
+	//The Main method in Java. Java looks for this method and then executes the code present under this method. 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My first Code in Java. 'Hello World'");
+	}
+
+}
